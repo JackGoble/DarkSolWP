69c6451f93f1aab57d2da9359d6950a966a9e38d
JackGoble <jackg.117@gmail.com>
2022-08-03 08:43:19 -0500

    fa testing

--- front-page.php
+++ front-page.php
@@ -8,8 +8,8 @@
     <script src="https://kit.fontawesome.com/435f5df33b.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Hello, world!<i class="fa-regular fa-atom-simple"></i></h1>
-    <i class="fa-regular fa-atom-simple"></i>
+    <h1>Hello, world!<i class="fa-solid fa-atom-simple"></i></h1>
+    <i class="fa-solid fa-atom-simple"></i>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
