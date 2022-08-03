c49da72be22cfb740ba780c720a8ac5e04e7ca8d
JackGoble <jackg.117@gmail.com>
2022-08-03 09:14:52 -0500

    meteor shower

--- front-page.php
+++ front-page.php
@@ -10,11 +10,11 @@
     <?php
     wp_head();
     ?>
-    
+
   </head>
   <body>
-    <h1>Hello, world!<i class="fa-solid fa-meteor"></i></h1>
-    <i class="fa-solid fa-meteor"></i>
+    <h1><i class="fa-solid fa-meteor"></i>Hello, world!<i class="fa-solid fa-meteor"></i></h1>
+    <i class="fa-solid fa-meteor"></i><i class="fa-solid fa-meteor"></i><i class="fa-solid fa-meteor"></i>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
