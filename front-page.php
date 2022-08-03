1897ceb113c46b672e6f63fd13cbf006020b1fc6
JackGoble <jackg.117@gmail.com>
2022-08-03 08:35:22 -0500

    testing fontawesome

--- front-page.php
+++ front-page.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/435f5df33b.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Hello, world!<i>atom-simple</i></h1>
+    <i>atom-simple</i>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
