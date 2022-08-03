d4ef046c567159fc605804e1f270679354e9fa15
JackGoble <jackg.117@gmail.com>
2022-08-03 08:47:26 -0500

    fa testing

--- front-page.php
+++ front-page.php
@@ -10,6 +10,7 @@
   <body>
     <h1>Hello, world!<i class="fa-solid fa-atom-simple"></i></h1>
     <i class="fa-solid fa-atom-simple"></i>
+    <i class="fa-solid fa-atom-simple"></i>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
