dee6918ccb790847b70c1d5d639be8356618431a
JackGoble <jackg.117@gmail.com>
2022-08-03 09:20:10 -0500

    test

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function jlg_register_files() {
-    wp_enqueue_style('jlg-bootstrap', get_template_path_directory_uri()."/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('jlg-bootstrap', get_template_path_directory_uri() . "/style.css", array(), '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'jlg-bootstrap');
